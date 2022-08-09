e707ca8c76ca8c705aed7967b5b1bdf6065b0c16
Harrison <harriwilson5534@gmail.com>
2022-08-09 16:45:52 -0400

    commit tree 052bf59707180422f8527c76617eb9554b41c4b1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push(name));
+}
+console.log(destructivelyAppendCat);
+
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift(name));
+}
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop());
+}
+function destructivelyRemoveFirstCat() {
+    console.log(cats.shift());
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.splice(-1);
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.splice(0, 1);
+    return copyOfCats;
+}
